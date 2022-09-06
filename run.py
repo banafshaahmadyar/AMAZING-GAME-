342c561f6d6bb8d1c5948176feeff87018b4b3b2
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2022-09-06 07:41:49 +0000

    added while loop for user choies

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+USER_NUM = 0
+PLAYER_NUM = 0
+select = ['rock' , ' scissor' , 'paper']
+
+while True:
+    user_select = input("please Enter your choies between ROCK...SCISSOR...PAPER=>")
+    if   user_select =="a":
+        break
+    
+
+
